5a1946e98f2cbeed09c34dfe422c04b2934c13a2
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-05-28 18:53:49 +0200

    python310Packages.ordereddict: drop

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6090,8 +6090,6 @@ in {
 
   opuslib = callPackage ../development/python-modules/opuslib { };
 
-  ordereddict = callPackage ../development/python-modules/ordereddict { };
-
   orderedmultidict = callPackage ../development/python-modules/orderedmultidict { };
 
   ordered-set = callPackage ../development/python-modules/ordered-set { };
